88f7d735fcc2ec067c759bc275a24099949bd8e0
vishnu6512 <vishnukainadan@gmail.com>
2025-01-29 15:51:51 +0530

    auth updated v2

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 // store.js
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './AuthSlice';
+import authReducer from './authenSlice';
 import chatReducer from './chatSlice';
 
 export default configureStore({
